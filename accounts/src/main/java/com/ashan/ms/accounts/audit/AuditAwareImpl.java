c35b45a0f003c8e8d30bb5511d09b9f732d7f469
niasha <nirakar.asha@capgemini.com>
2024-09-24 11:57:22 +0530

    add audit for account and customer entity

---
+++ accounts/src/main/java/com/ashan/ms/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,18 @@
+package com.ashan.ms.accounts.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /**
+     * @return the current auditor of the application
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNTS_MS");
+    }
+}
--- accounts/src/main/java/com/ashan/ms/accounts/service/impl/AccountServiceImpl.java
+++ accounts/src/main/java/com/ashan/ms/accounts/service/impl/AccountServiceImpl.java
@@ -15,7 +15,6 @@ import com.ashan.ms.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 
@@ -37,8 +36,6 @@ public class AccountServiceImpl implements IAccountsService {
         if( optionalCustomer.isPresent() ) {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobile number "+customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -55,8 +52,6 @@ public class AccountServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
