030b303b6888f694eba72661cab977d6aeb5b2ea
niasha <nirakar.asha@capgemini.com>
2024-10-01 12:20:54 +0530

    add loans rest services

--- loans/src/main/java/com/ashan/ms/loans/LoansApplication.java
+++ loans/src/main/java/com/ashan/ms/loans/LoansApplication.java
@@ -1,9 +1,30 @@
 package com.ashan.ms.loans;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loan microservice REST API documentation",
+				description = "Bank Loan microservice REST API documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Nirakar Asha",
+						email = "nirakar.asha@gmail.com"
+				),
+				license = @License(
+						name = "MIT",
+						url = "https://opensource.org/license/mit"
+				)
+		)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
---
+++ loans/src/main/java/com/ashan/ms/loans/dto/ResponseDto.java
@@ -0,0 +1,23 @@
+package com.ashan.ms.loans.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Schema(
+        name = "Response",
+        description = "Schema to hold successful response"
+)
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+    @Schema(
+            description = "Status code of response"
+    )
+    private String statusCode;
+
+    @Schema(
+            description = "Status message of response"
+    )
+    private String statusMessage;
+}
