2e5a127010e55bb496a11ef038b58a2356d939d9
niasha <nirakar.asha@capgemini.com>
2024-10-03 13:17:33 +0530

    add cards rest services

---
+++ cards/src/main/java/com/ashan/ms/cards/audit/AuditAwareImpl.java
@@ -0,0 +1,18 @@
+package com.ashan.ms.cards.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /**
+     * @return the current auditor of the application
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("CARDS_MS");
+    }
+}
--- cards/src/main/java/com/ashan/ms/cards/constants/CardsConstants.java
+++ cards/src/main/java/com/ashan/ms/cards/constants/CardsConstants.java
@@ -1,19 +1,21 @@
-package com.ashan.ms.loans.constants;
+package com.ashan.ms.cards.constants;
 
-public class LoansConstants {
-    private LoansConstants() {
+public final class CardsConstants {
+
+    private CardsConstants() {
         // restrict instantiation
     }
 
-    public static final String  HOME_LOAN = "Home Loan";
-    public static final int     NEW_LOAN_LIMIT = 1_00_000;
+    public static final String  CREDIT_CARD = "Credit Card";
+    public static final int  NEW_CARD_LIMIT = 1_00_000;
     public static final String  STATUS_201 = "201";
-    public static final String  MESSAGE_201 = "Loan created successfully";
+    public static final String  MESSAGE_201 = "Card created successfully";
     public static final String  STATUS_200 = "200";
     public static final String  MESSAGE_200 = "Request processed successfully";
     public static final String  STATUS_417 = "417";
     public static final String  MESSAGE_417_UPDATE= "Update operation failed. Please try again or contact Support team";
     public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again or contact Support team";
-    public static final String  STATUS_500 = "500";
-    public static final String  MESSAGE_500 = "An error occurred. Please try again or contact Support team";
+     public static final String  STATUS_500 = "500";
+     public static final String  MESSAGE_500 = "An error occurred. Please try again or contact Support team";
+
 }
